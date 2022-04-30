deeb3b11e96997687fd3a27b1bc895412da2b3d3
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-04-29 21:24:07 -0400

    Fixes
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- blueprints/apis/server_ns.py
+++ blueprints/apis/server_ns.py
@@ -7,12 +7,12 @@ from classes.shared import db
 api = Namespace('server', description='Server Related Queries and Functions')
 
 serverAdd = reqparse.RequestParser()
-serverAdd.add_argument('address', type=str, required=True)
-serverAdd.add_argument('protocol', type=str, required=True)
+serverAdd.add_argument('address', type=str, required=True, location='form')
+serverAdd.add_argument('protocol', type=str, required=True, location='form')
 
 serverDelete = reqparse.RequestParser()
-serverDelete.add_argument('id', type=str, required=True)
-serverDelete.add_argument('token', type=str, required=True)
+serverDelete.add_argument('id', type=str, required=True, location='form')
+serverDelete.add_argument('token', type=str, required=True, location='form')
 
 @api.route('/')
 class api_server_root(Resource):
