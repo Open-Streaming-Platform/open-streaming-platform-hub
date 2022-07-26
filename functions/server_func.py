938e4182ebfd1e1947b27a006d5db8ccfb9e7d31
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-07-26 07:50:48 -0400

    Add basic confirmation

--- functions/server_func.py
+++ functions/server_func.py
@@ -6,7 +6,7 @@ def verifyServer(serverId):
     return_confirmation = False
     server_query = servers.server.query.filter_by(id=serverId).first()
     if server_query is not None:
-        r=requests.get(server_query.get_Url())
+        r=requests.get(server_query.get_Url() + '/apiv1/server/')
         if r.status_code == 200:
             settings = r.json()['results']
             # Add check to verify current hub url and if 
