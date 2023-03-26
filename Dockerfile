15a438b66b36fc12412ae0a6b10c80f5c2dded26
Dave Lockwood <dave.r.lockwood@gmail.com>
2023-03-26 14:43:36 -0400

    Add Celery Format

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,12 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+# Install Supervisor
+RUN apt install -y supervisor
+RUN mkdir -p /var/log/supervisor
+
+RUN mkdir /app/logs
+
 # Copy the current directory contents into the container at /app
 COPY . /app
 RUN chmod +x /app/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 cd /app
+echo 'Performing DB Migrations'
 flask db upgrade
-python3 app.py
+
+echo 'Fixing OSP Permissions Post Migration'
+chown -R www-data:www-data /app
+
+export OSP_CORE_TYPE
+
+echo "Starting OSP-$OSP_CORE_TYPE"
+case "$OSP_CORE_TYPE" in
+ celery) supervisord --nodaemon --configuration /app/docker-files.d/supervisord-celery.conf ;;
+ beat) supervisord --nodaemon --configuration /app/docker-files.d/supervisord-celery-beat.conf ;;
+    *) supervisord --nodaemon --configuration /app/docker-files.d/supervisord.conf ;;
+esac
